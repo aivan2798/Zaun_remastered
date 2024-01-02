10a4f81d946f5e76faadd93c09d6992c29dddb20
aivan2798 <cot2we@gmail.com>
2024-01-02 03:47:08 +0000

    started working on zion

---
+++ Zion/make_zion.sh
@@ -0,0 +1,2 @@
+g++ zion.cpp -o zion
+./zion
---
+++ Zion/zion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "zaun.h"
+
+using namespace std;
+int main()
+{
+    cout<<"\t\t_________________________________________________\n\t\t|\tZION, THE LEGENDARY CITY OF FREEDOM\t|";
+    cout<<"\n\t\t_________________________________________________";
+
+    uint16_t zion_main_port;
+    cout<<endl<<"-->Enter Main Port: ";
+    cin>>zion_main_port;
+    cout<<endl<<"<--Receiving on safe port: "<<zion_main_port<<endl;
+
+}
